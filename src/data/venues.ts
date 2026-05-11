f8acd98e724460c380d6b0623fe34ef2408cef3b
Makobelat <89588444+Makobelat@users.noreply.github.com>
2026-05-11 14:17:07 +0200

    Update venues.ts

--- src/data/venues.ts
+++ src/data/venues.ts
@@ -5,7 +5,7 @@ export const venues = [
     description: 'An elegant space perfect for large conferences and formal events',
     capacity: 500,
     pricePerHour: 1000,
-    imageUrl: '/src/assets/KPimages/downvip.jpg',
+    imageUrl: '../assets/KPimages/Kgalala images  13.jpg',
     amenities: ['Stage', 'Professional Sound System', 'LED Screens', 'Banquet Setup']
   },
   {
